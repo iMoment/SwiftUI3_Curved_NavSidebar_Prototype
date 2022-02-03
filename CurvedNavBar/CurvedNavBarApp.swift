a466eb7dd0d16546d805571ba8d4827894f15086
Stanley Pan <imoment.developer@gmail.com>
2022-02-03 15:41:03 -0500

    Initial Commit

---
+++ CurvedNavBar/CurvedNavBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurvedNavBarApp.swift
+//  CurvedNavBar
+//
+//  Created by Stanley Pan on 2022/02/03.
+//
+
+import SwiftUI
+
+@main
+struct CurvedNavBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
